59c1e40f098d8586e4c64d5ba44914c7ecee9df2
Dilynn Strickland <dilynn.strickland@flowershopnetwork.com>
2025-10-27 10:57:38 -0500

    Exercise 1 Superheader Complete

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -13,23 +13,38 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      <InnerWrapper>
+        <SearchInput />
+        <HelpLink href="/help">Help</HelpLink>
+        <UnstyledButton>
+          <Icon id="shopping-bag" strokeWidth={1} />
+        </UnstyledButton>
+      </InnerWrapper>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  width: 100%;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  height: 40px;
+  align-items: center;
+`;
+
+const InnerWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 25%;
 `;
 
 const MarketingMessage = styled.span`
+  width: 75%;
   color: ${COLORS.white};
+  padding-left: 32px;
 `;
 
 const HelpLink = styled.a`
