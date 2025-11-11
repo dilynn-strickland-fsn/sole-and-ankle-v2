8890ec1f5992a412e3893659a914309b43a5f4d3
Dilynn Strickland <dilynn.strickland@flowershopnetwork.com>
2025-11-11 13:14:49 -0600

    Exercise 4 Part A and B solution follow along

--- src/components/ShoeCard/ShoeCard.jsx
+++ src/components/ShoeCard/ShoeCard.jsx
@@ -36,14 +36,20 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+          {variant === 'new-release' && <NewFlag>Just Released!</NewFlag>}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price style={{
+            '--color': variant === 'on-sale' ? COLORS.gray[700] : undefined,
+            '--text-decoration' : variant === 'on-sale' ? 'line-through' : undefined,
+            }}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          { variant === 'on-sale' ? <SalePrice>{formatPrice(salePrice)}</SalePrice> : undefined }
         </Row>
       </Wrapper>
     </Link>
@@ -51,29 +57,22 @@ const ShoeCard = ({
 };
 
 const Link = styled.a`
-  display: flex;
-  flex-direction: column;
-  min-height: 370px;
   text-decoration: none;
   color: inherit;
-  max-width: 344px;
 `;
 
 const Wrapper = styled.article`
-  flex: 1;
 `;
 
 const ImageWrapper = styled.div`
   position: relative;
-  height: 300px;
   background-color: ${COLORS.gray[100]};
-  border: 1px transparent;
   border-radius: 16px 16px 4px 4px;
+  overflow: hidden;
 `;
 
 const Image = styled.img`
-  width: 340px;
-  border-radius: 16px 16px;
+  width: 100%;
 `;
 
 const Row = styled.div`
@@ -88,7 +87,8 @@ const Name = styled.h3`
 `;
 
 const Price = styled.span`
-  color: ${COLORS.gray[700]};
+  color: var(--color);
+  text-decoration: var(--text-decoration);
 `;
 
 const ColorInfo = styled.p`
@@ -98,6 +98,27 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+`;
+
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  color: ${COLORS.white};
+  height: 32px;
+  line-height: 32px;
+  padding: 0 10px;
+  font-size: ${14 / 16}rem;
+  font-weight: ${WEIGHTS.bold};
+  border-radius: 2px;
+`;
+
+const SaleFlag = styled(Flag)`
+  background-color: ${COLORS.primary};
+`;
+
+const NewFlag = styled(Flag)`
+  background-color: ${COLORS.secondary};
 `;
 
 export default ShoeCard;
--- src/components/ShoeGrid/ShoeGrid.jsx
+++ src/components/ShoeGrid/ShoeGrid.jsx
@@ -8,7 +8,9 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
@@ -16,9 +18,13 @@ const ShoeGrid = () => {
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
   flex-wrap: wrap;
-  gap: 36px;
+  gap: 32px;
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  flex: 1;
 `;
 
 export default ShoeGrid;
